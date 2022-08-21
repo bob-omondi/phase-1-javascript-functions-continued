10e665a6fe9536430760db8d8ed37a32a4e4494b
+ <+>
2022-08-21 22:33:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,48 +1,17 @@
-// code your solution here
-function saturdayFun(defaul="roller-skate"){
-    console.log("This Saturday, I want to ${saturdayFun}!")
-}
-saturdayFun();
-
-function mondayWork(activity="go to the office"){
-    console.log("This Monday, I will ${mondayWork} .");
-}
-mondayWork();
-
-// Implement a function called saturdayFun:
-
-// It should define a function
-// It uses a default argument, 'roller-skate' when no arguments are passed
-// allows the default argument to be overridden
-
-function saturdayFun() {
-    return function (activity='roller-skate') {
-      return `This Saturday, I want to ${activity} !`;
-    };
-  }
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+    }
+    saturdayFun()
+    
+    let mondayWork = function(place="go to the office"){
+     return `This Monday, I will ${place}.` 
+    }
+    mondayWork()
+    
+    function wrapAdjective (operator="*") {
+      return function(stringOne="special"){
+        return `You are ${operator}${stringOne}${operator}!`
+      }
+    }
   
-  saturdayFun()();
-
-function saturdayFun() {
-    return function (activity='roller-skate') {
-      return `This Saturday, I want to ${activity} !`;
-    };
-  }
-  
-  saturdayFun()("bathe my dog");
-
-  function mondayWork() {
-    return function (action ='go to the office') {
-        return `This Saturday, I want to ${action}.`;
-    };
-  }
-  
-  mondayWork(work)(" work from home");
-  
-  function wrapAdjective(remainder) {
-    const innerFunction = function (name= "special" ) {
-      return `You are ${remainder} a ${name} ${remainder} ! `;
-    };
-    return innerFunction;
-  }
-  wrapAdjective("%")("a dedicated programmer");
+    
